f5b601ff13649ed1753549c7eeab7aea2ea860e8
Baumi <baumgarb@gmail.com>
2021-01-09 17:10:34 +0100

    02.1.1: read button input once per second instead of constantly

--- 02.1.1_ButtonLED/main.go
+++ 02.1.1_ButtonLED/main.go
@@ -2,26 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/stianeikeland/go-rpio"
 )
 
 func main() {
 	ledPinNr := 17
 	buttonPinNr := 18
-	
+
 	err := rpio.Open()
 	defer rpio.Close()
-	
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	ledPin := rpio.Pin(ledPinNr)
 	ledPin.Output()
 	buttonPin := rpio.Pin(buttonPinNr)
 	buttonPin.Input()
 	buttonPin.PullUp()
-		
+
 	for {
 		input := buttonPin.Read()
 		if input == rpio.Low {
@@ -29,5 +31,8 @@ func main() {
 		} else {
 			ledPin.Low()
 		}
+
+		time.Sleep(time.Second)
+		fmt.Println("Input: ", input)
 	}
 }
